e344e33a7f810cb877d5900f19d81e150b1b448d
Greg Friedland <greg.friedland@gmail.com>
2022-06-02 20:51:44 -0700

    fix

--- main_torch.py
+++ main_torch.py
@@ -195,8 +195,8 @@ def train(adversarial_loss, dataloader, discriminator: Discriminator, generator:
 
 def main():
     parser = argparse.ArgumentParser()
-    parser.add_argument("--n_epochs", type=int, default=200, help="number of epochs of training")
-    parser.add_argument("--batch_size", type=int, default=4096, help="size of the batches")
+    parser.add_argument("--n_epochs", type=int, default=25, help="number of epochs of training")
+    parser.add_argument("--batch_size", type=int, default=2048, help="size of the batches")
     parser.add_argument("--lr", type=float, default=0.0002, help="adam: learning rate")
     parser.add_argument("--b1", type=float, default=0.5, help="adam: decay of first order momentum of gradient")
     parser.add_argument("--b2", type=float, default=0.999, help="adam: decay of first order momentum of gradient")
